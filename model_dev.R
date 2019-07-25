0253369b794909b69bce8ba0adc1d4e4b8a3bfd8
nguyen7594 <49255510+nguyen7594@users.noreply.github.com>
2019-07-24 22:32:28 -0500

    nba analysis file: code updated for R

--- model_dev.R
+++ model_dev.R
@@ -5,6 +5,10 @@ library(ggplot2)
 library(corrplot)
 library(purrr)
 library(stringr)
+#install.packages('splitstackshape')
+library(splitstackshape)
+library(caret)
+
 
 set.seed(1234)
 ## Import csv files
@@ -46,13 +50,65 @@ colSums(is.na(set_x))
 set_x[is.na(set_x)] <- 0
 
 # Position 
-unique(set_x$Pos)
-set_x[,"Pos"]%>%
-  
+#unique(set_x$Pos)
+pos_sep <- data.frame(all_set$Player_,set_x$Pos)
+#head(pos_sep)
+sepPos <- cSplit(pos_sep,'set_x.Pos',sep='-')
+sepPos$pos.PG <- 0
+sepPos$pos.SG <- 0
+sepPos$pos.SF <- 0
+sepPos$pos.PF <- 0
+sepPos$pos.C <- 0
+sepPos[(sepPos$set_x.Pos_1=='PG')|(sepPos$set_x.Pos_2=='PG'),'pos.PG']<-1
+sepPos[(sepPos$set_x.Pos_1=='SG')|(sepPos$set_x.Pos_2=='SG'),'pos.SG']<-1
+sepPos[(sepPos$set_x.Pos_1=='SF')|(sepPos$set_x.Pos_2=='SF'),'pos.SF']<-1
+sepPos[(sepPos$set_x.Pos_1=='PF')|(sepPos$set_x.Pos_2=='PF'),'pos.PF']<-1
+sepPos[(sepPos$set_x.Pos_1=='C')|(sepPos$set_x.Pos_2=='C'),'pos.C']<-1
+#head(sepPos)
+set_x <- data.frame(set_x[,-1],sepPos[,c('pos.PG','pos.SG','pos.SF','pos.PF','pos.C')])
+names(set_x)
+
+### Split the training-valid-set
+set.seed(1234)
+df <- data.frame(all_set$Year,1:nrow(set_x))
+df_index <-sample(1:nrow(set_x))
+#head(df_index)
+df <- df[df_index,]
+#head(df)
+trainvalid_ <-  createDataPartition(df$all_set.Year, times = 1, p = 0.8, list = FALSE)
+# test data index
+test_index <- df[-train_x_index,]$X1.nrow.set_x.
+#head(test_index)
+# train-valid data index
+trainvalid_index <- data.frame(index=df[train_x_index,]$X1.nrow.set_x.,year=df[train_x_index,]$all_set.Year)
+#head(trainvalid_index)
+train_ <- createDataPartition(trainvalid_index$year, times = 1, p = 0.8, list = FALSE)
+# train index
+train_index <- trainvalid_index[train_,]$index
+#head(train_index)
+# valid index
+valid_index <- trainvalid_index[-train_,]$index
+#head(valid_index)
+
+## Split the data 
+# train df
+train_xdf <- set_x[train_index,] 
+train_ydf <- set_y[train_index,]
+#nrow(train_xdf)
+# valid df
+valid_xdf <- set_x[valid_index,] 
+valid_ydf <- set_y[valid_index,]
+#nrow(valid_xdf)
+# test df
+test_xdf <- set_x[test_index,] 
+test_ydf <- set_y[test_index,]
+#nrow(test_xdf)
+
+
 
 
 #cor bw WS and predictor variables
-cor_xy <- cor(train_y,train_x[,-1],use='pairwise.complete.obs')
+cor_xy <- cor(train_y,train_x[,-c('pos.PG','pos.SG','pos.SF','pos.PF','pos.C')],use='pairwise.complete.obs')
 cor_xy
 corrplot(cor_xy)
 #cor bw predictor variables
@@ -64,5 +120,7 @@ corrplot(cor_x)
 ##Visualization
 ggplot(train_set,aes(PTS,WS2))+
   geom_jitter(aes(size=PF))
+
+
 
 ## Dimensional Reduction
