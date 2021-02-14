d59fc6bd3e73b4a5c13e675e1eaba35ecf7be574
nguyen7594 <49255510+nguyen7594@users.noreply.github.com>
2021-02-14 15:18:21 +0700

    Update source files for data analysis and model development

--- nba_analysis.py
+++ nba_analysis.py
@@ -25,7 +25,7 @@ from sklearn.impute import SimpleImputer
 ## YEAR - ALL_STAR_LIST THE FIRST YEAR OF THAT SEASON
 file_1 = 'Seasons_Stats.csv'
 file_2 = 'NBA_All_Star_Games.csv'
-file_3 = 'NBA_All_Star_Games_addition.csv'
+file_3 = 'NBA_All_Star_Games_195051to201718.csv'
 
 # Github load
 file_path_github = 'https://raw.githubusercontent.com/nguyen7594/isqs5381_summer19nn/master/src/'
@@ -50,7 +50,7 @@ all_star.Year = all_star.Year + 1
 # remove 1999 all star game: error - year without all-star
 all_star = all_star[all_star['Year'] != 1999]
 # 895 vabs
-# all_star[all_star['Year'] == 2018].count()
+all_star[all_star['Year'] == 2018].count()
 # 2018: 28 obs => rest = 895-28 = 867 obs
 
 
@@ -90,37 +90,16 @@ all_star = all_star[vabs_selected_as]
 ## JOIN ALL_STAR WITH SEASON_STATS
 season_stat_ = pd.merge(season_stat,all_star,left_on=['Year','Player_'],right_on=['Year','Player'],how='left')
 #season_stat_.info()
-# 853 obs => 14 missing values from all_star
 
 # players would not be matchaed 
 #left_out_ = pd.merge(season_stat,all_star,left_on=['Year','Player_'],right_on=['Year','Player'],how='right')
 #left_out_.info()
-# 14 not matched 
-#left_out_[(left_out_['Player_x'].isnull())&(left_out_['Year']!=2018)][['Player_y','Year']]
+#left_out_[(left_out_['Player_x'].isnull())&(left_out_['Year']<2018)][['Player_y','Year']]
 
-## FILL MISSING VALUES FOR ALL_STAR vabs: 14 NAs
-# Metta World 2004 - 1
-season_stat_.loc[(season_stat_['Player_']=='Metta World')&(season_stat_['Year']==2004),'all_star'] = 1 
-# Maurice Cheeks 1983,1986,1987,1988 - 4
-season_stat_.loc[(season_stat_['Player_']=='Maurice Cheeks')&(season_stat_['Year'].isin([1983,1986,1987,1988])),'all_star'] = 1 
-season_stat_.loc[(season_stat_['Player_']=='Maurice Cheeks')&(season_stat_['Year'].isin([1983,1986,1987,1988])),'all_star']
-# Micheal Ray 1985 - 1
-season_stat_.loc[(season_stat_['Player_']=='Micheal Ray')&(season_stat_['Year']==1985),'all_star'] = 1 
-# Joe Barry 1987 - 1
-season_stat_.loc[(season_stat_['Player_']=='Joe Barry')&(season_stat_['Year']==1987),'all_star'] = 1 
-# Magic Johnson did not play 1992 - 1
-# Clifford Robinson 1994 - 1
-season_stat_.loc[(season_stat_['Player_']=='Clifford Robinson')&(season_stat_['Year']==1994),'all_star'] = 1 
-# Anfernee Hardway 1995,1996,1997,1998 - 4
-season_stat_.loc[(season_stat_['Player_']=='Anfernee Hardaway')&(season_stat_['Year'].isin([1995,1996,1997,1998])),'all_star'] = 1 
-# Nick Van 1998 - 1
-season_stat_.loc[(season_stat_['Player_']=='Nick Van')&(season_stat_['Year']==1998),'all_star'] = 1 
-# double check the count: 866 + 1
-#season_stat_.info()
+
 # fill others as 0
 season_stat_['all_star'].fillna(0,inplace=True)
 #season_stat_.info()
-
 
 
 ### Final csv file output
@@ -129,10 +108,8 @@ vabs_selected_output = ['Year','Player_','Tm','Pos','G','MP','Age','PTS','FG','F
                   'DRB','DRB%','ORB','ORB%','STL','STL%',
                   'TOV','TOV%','PF','WS','all_star']
 season_stat_ = season_stat_[vabs_selected_output]
-#season_stat_.to_csv(os.path.join(file_path_local,r'nba_stat_merged.csv'),index=False)
+file_path_local = 'C:\\Users\\nguye\\Documents\\TTU\\5381\\nba'
 season_stat_.to_csv(os.path.join(file_path_local,r'isqs5381_summer19nn',r'src',r'nba_stat_merged.csv'),index=False)
-
-
 
 
 
